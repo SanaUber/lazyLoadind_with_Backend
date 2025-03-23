f8f45c921307f59d46d27c0f64bb2a2862184324
SanaGithubUber <sanagithububer@gmail.com>
2025-03-23 11:17:07 +0400

    EmployeeApi

---
+++ EmployeeApis/Controller/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApi.Models;
+ 
+using Microsoft.EntityFrameworkCore;
+using EmployeeApis.Data;
+
+
+namespace EmployeeApi.Controller
+{
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class EmployeesController : ControllerBase
+        {
+            private readonly AppDbContext _context;
+
+            public EmployeesController(AppDbContext context)
+            {
+                _context = context;
+            }
+        // GET: api/employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+    }
+}
---
+++ EmployeeApis/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Salary must be positive.")]
+        public decimal Salary { get; set; }
+
+        
+    }
+}
